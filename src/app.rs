19e92b3418d8d4a42f190c4fdf8e98a8a0492464
Christian Krause <christian.krause@mailbox.org>
2019-02-05 22:30:01 +0100

    bumps copyright year

--- src/app.rs
+++ src/app.rs
@@ -1,3 +1,29 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                                           *
+ *  Copyright  (C)  2015-2019  Christian Krause                              *
+ *                                                                           *
+ *  Christian Krause  <christian.krause@mailbox.org>                         *
+ *                                                                           *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                                           *
+ *  This file is part of strace-analyzer.                                    *
+ *                                                                           *
+ *  strace-analyzer is free software: you can redistribute it and/or modify  *
+ *  it under the terms of the GNU General Public License as published by     *
+ *  the Free Software Foundation, either version 3 of the license, or any    *
+ *  later version.                                                           *
+ *                                                                           *
+ *  strace-analyzer is distributed in the hope that it will be useful, but   *
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of               *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU         *
+ *  General Public License for more details.                                 *
+ *                                                                           *
+ *  You should have received a copy of the GNU General Public License along  *
+ *  with strace-analyzer. If not, see <http://www.gnu.org/licenses/>.        *
+ *                                                                           *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+
 use atty::Stream;
 use clap::{App, AppSettings, Arg};
 use std::path::Path;
