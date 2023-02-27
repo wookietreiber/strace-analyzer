f6c6b8d20eef9d263ef7657c9fc87c2733d3f87e
Christian Krause <christian.krause@mailbox.org>
2023-02-27 11:50:54 +0100

    bumps copyright year

--- src/output.rs
+++ src/output.rs
@@ -1,6 +1,6 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  *                                                                           *
- *  Copyright  (C)  2015-2022  Christian Krause                              *
+ *  Copyright  (C)  2015-2023  Christian Krause                              *
  *                                                                           *
  *  Christian Krause  <christian.krause@mailbox.org>                         *
  *                                                                           *
